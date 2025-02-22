ccc79820bff54956ea22eff64e994d6216c00f65
omkarP-r <omkar29pawar@gmail.com>
2025-02-22 22:22:40 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './component/common/Navbar';
+import FooterComponent from './component/common/Footer';
+import HomePage from './component/home/HomePage';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Navbar/>
+
+     <div className='content'>
+      <Routes>
+      <Route exact path="/home" element={<HomePage />} />
+
+      </Routes>
+
+     </div>
+
+     <FooterComponent/>
+
     </div>
+    </BrowserRouter>
   );
 }
 
