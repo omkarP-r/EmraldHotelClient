549e3eec59ec3110a407f3627e54543e852d2ade
omkarP-r <omkar29pawar@gmail.com>
2025-02-23 17:52:22 +0530

    nav

--- src/component/common/Navbar.jsx
+++ src/component/common/Navbar.jsx
@@ -25,6 +25,13 @@ function Navbar() {
                 <li><NavLink to="/home" activeclassname="active">Home</NavLink></li>
                 <li><NavLink to="/rooms" activeclassname="active">Rooms</NavLink></li>
                 <li><NavLink to="/find-booking" activeclassname="active">Find my Booking</NavLink></li>
+                
+                {isUser && <li><NavLink to="/profile" activeclassname="active">Profile</NavLink></li>}
+                {isAdmin && <li><NavLink to="/admin" activeclassname="active">Admin</NavLink></li>}
+
+                {!isAuthenticated &&<li><NavLink to="/login" activeclassname="active">Login</NavLink></li>}
+                {!isAuthenticated &&<li><NavLink to="/register" activeclassname="active">Register</NavLink></li>}
+                {isAuthenticated && <li onClick={handleLogout}>Logout</li>}
 
             </ul>
         </nav>
