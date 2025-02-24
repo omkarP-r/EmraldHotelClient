2f90f54a42c3aa84f450af143656436e15cd9e8f
omkarP-r <omkar29pawar@gmail.com>
2025-02-24 19:29:08 +0530

    component

--- src/component/common/Footer.jsx
+++ src/component/common/Footer.jsx
@@ -4,7 +4,7 @@ const FooterComponent = () => {
     return (
         <footer>
             <span className="my-footer">
-                Phegon Hotel | All Right Reserved &copy; {new Date().getFullYear()}
+                Emrald Hotel | All Right Reserved &copy; {new Date().getFullYear()}
             </span>
         </footer>
     );
