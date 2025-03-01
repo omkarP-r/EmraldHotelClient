a6e6ebcc918ba12b167ef638a2d5f6d0cd653350
omkarP-r <omkar29pawar@gmail.com>
2025-03-01 21:30:21 +0530

    apiservice

---
+++ src/service/ApiService.js
@@ -0,0 +1,32 @@
+import axios from "axios"
+
+export default class ApiService {
+
+    static BASE_URL = "http://localhost:8080"
+
+    static getHeader() {
+        const token = localStorage.getItem("token");
+        return {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json"
+        };
+    }
+
+    /**AUTH */
+
+    /* This  register a new user */
+    static async registerUser(registration) {
+        const response = await axios.post(`${this.BASE_URL}/auth/register`, registration)
+        return response.data
+    }
+
+    /* This  login a registered user */
+    static async loginUser(loginDetails) {
+        const response = await axios.post(`${this.BASE_URL}/auth/login`, loginDetails)
+        return response.data
+    }
+
+    /***USERS */
+
+
+}
