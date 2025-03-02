b2ed19e7b7946a6038fb2c77eb4cadf9451aba48
omkarP-r <omkar29pawar@gmail.com>
2025-03-02 18:25:55 +0530

    room result

---
+++ src/component/common/RoomResult.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate
+import ApiService from '../../service/ApiService';
+
+const RoomResult = ({ roomSearchResults }) => {
+    const navigate = useNavigate(); // Initialize useNavigate hook
+    const isAdmin = ApiService.isAdmin();
+    return (
+        <section className="room-results">
+           
+        </section>
+    );
+}
+
+export default RoomResult;
