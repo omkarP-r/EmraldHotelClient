5ebb9ba5af92c60934d17dd35ba13d999f8d5825
omkarP-r <omkar29pawar@gmail.com>
2025-03-03 17:56:22 +0530

    room

--- src/component/common/RoomResult.jsx
+++ src/component/common/RoomResult.jsx
@@ -7,7 +7,23 @@ const RoomResult = ({ roomSearchResults }) => {
     const isAdmin = ApiService.isAdmin();
     return (
         <section className="room-results">
-           
+            {roomSearchResults && roomSearchResults.length > 0 && (
+                <div className="room-list">
+                    {roomSearchResults.map(room => (
+                        <div key={room.id} className="room-list-item">
+                            <img className='room-list-item-image' src={room.roomPhotoUrl} alt={room.roomType} />
+                            <div className="room-details">
+                                <h3>{room.roomType}</h3>
+                                <p>Price: ${room.roomPrice} / night</p>
+                                <p>Description: {room.roomDescription}</p>
+                            </div>
+
+                           
+
+                        </div>
+                    ))}
+                </div>
+            )}
         </section>
     );
 }
