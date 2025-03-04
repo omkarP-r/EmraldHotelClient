c0cbd0b16bbafc382c605c2da2df8e6231b80151
omkarP-r <omkar29pawar@gmail.com>
2025-03-04 19:42:33 +0530

    header section

--- src/component/home/HomePage.jsx
+++ src/component/home/HomePage.jsx
@@ -18,6 +18,20 @@ const HomePage = () => {
 
     return (
         <div className="home">
+            {/* HEADER / BANNER ROOM SECTION */}
+            <section>
+                <header className="header-banner">
+                    <img src="./assets/images/hotel.webp" alt="Phegon Hotel" className="header-image" />
+                    <div className="overlay"></div>
+                    <div className="animated-texts overlay-content">
+                        <h1>
+                            Welcome to <span className="phegon-color">Hotel Emrald</span>
+                        </h1><br />
+                        <h3>Step into a haven of comfort and care</h3>
+                    </div>
+                </header>
+            </section>
+
            
         </div>
     );
