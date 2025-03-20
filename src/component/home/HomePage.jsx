d09b5c8b3a8c0ab89511ad45a04ef7fcefc7c46f
omkarP-r <omkar29pawar@gmail.com>
2025-03-20 19:34:47 +0530

    home page

--- src/component/home/HomePage.jsx
+++ src/component/home/HomePage.jsx
@@ -31,7 +31,35 @@ const HomePage = () => {
                     </div>
                 </header>
             </section>
+ {/* SEARCH/FIND AVAILABLE ROOM SECTION */}
+ <RoomSearch handleSearchResult={handleSearchResult} />
+            <RoomResult roomSearchResults={roomSearchResults} />
 
+            <h4><a className="view-rooms-home" href="/rooms">All Rooms</a></h4>
+
+            <h2 className="home-services">Services at <span className="phegon-color">Hotel Emrald</span></h2>
+
+            {/* SERVICES SECTION */}
+            <section className="service-section"><div className="service-card">
+                <img src="./assets/images/ac.png" alt="Air Conditioning" />
+                <div className="service-details">
+                    <h3 className="service-title">Air Conditioning</h3>
+                    <p className="service-description">Stay cool and comfortable throughout your stay with our individually controlled in-room air conditioning.</p>
+                </div>
+            </div>
+                <div className="service-card">
+                    <img src="./assets/images/mini-bar.png" alt="Mini Bar" />
+                    <div className="service-details">
+                        <h3 className="service-title">Mini Bar</h3>
+                        <p className="service-description">Enjoy a convenient selection of beverages and snacks stocked in your room's mini bar with no additional cost.</p>
+                    </div>
+                </div>
+                
+            </section>
+            {/* AVAILABLE ROOMS SECTION */}
+            <section>
+
+            </section>
            
         </div>
     );
