4d6d6cbfb4a198d36e3c90f2354122d0ff9771ee
omkarP-r <omkar29pawar@gmail.com>
2025-03-21 19:12:16 +0530

    room search

---
+++ src/component/common/RoomSearch.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import ApiService from '../../service/ApiService';
+
+const RoomSearch = ({ handleSearchResult }) => {
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const [roomType, setRoomType] = useState('');
+  const [roomTypes, setRoomTypes] = useState([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const fetchRoomTypes = async () => {
+      try {
+        const types = await ApiService.getRoomTypes();
+        setRoomTypes(types);
+      } catch (error) {
+        console.error('Error fetching room types:', error.message);
+      }
+    };
+    fetchRoomTypes();
+  }, []);
+
+  
+
+export default RoomSearch;
