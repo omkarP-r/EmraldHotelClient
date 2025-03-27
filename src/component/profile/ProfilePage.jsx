8bd32f20cc2edabd4992ef3bcbaa2f422960fcb0
omkarP-r <omkar29pawar@gmail.com>
2025-03-27 12:12:48 +0530

    profile

--- src/component/profile/ProfilePage.jsx
+++ src/component/profile/ProfilePage.jsx
@@ -31,7 +31,15 @@ const ProfilePage = () => {
     const handleEditProfile = () => {
         navigate('/edit-profile');
     };
-
+    return (
+        <div className="profile-page">
+            {user && <h2>Welcome, {user.name}</h2>}
+            <div className="profile-actions">
+                <button className="edit-profile-button" onClick={handleEditProfile}>Edit Profile</button>
+                <button className="logout-button" onClick={handleLogout}>Logout</button>
+            </div>
+            
+    );  
     
 };
 
