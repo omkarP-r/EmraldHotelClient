ef028711420494b88bf2fb03834553e8aae5addc
omkarP-r <omkar29pawar@gmail.com>
2025-03-28 15:40:34 +0530

    edit profile

---
+++ src/component/profile/EditProfilePage.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import ApiService from '../../service/ApiService';
+
+const EditProfilePage = () => {
+    const [user, setUser] = useState(null);
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
+
+   
