92cbfecb169b3beeda0be6119b2f5583563d358c
omkarP-r <omkar29pawar@gmail.com>
2025-03-30 17:58:39 +0530

    rooms

--- src/component/common/Pagination.jsx
+++ src/component/common/Pagination.jsx
@@ -3,6 +3,11 @@ import React from 'react';
 const Pagination = ({ roomsPerPage, totalRooms, currentPage, paginate }) => {
   const pageNumbers = [];
 
+  for (let i = 1; i <= Math.ceil(totalRooms / roomsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
  
+};
 
 export default Pagination;
