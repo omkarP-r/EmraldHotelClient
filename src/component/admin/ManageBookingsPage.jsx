af84e3788952289c3a1c9cef162abf85a6de0352
omkarP-r <omkar29pawar@gmail.com>
2025-04-16 18:52:38 +0530

    manage booking

---
+++ src/component/admin/ManageBookingsPage.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import ApiService from '../../service/ApiService';
+import Pagination from '../common/Pagination';
+
+const ManageBookingsPage = () => {
+    const [bookings, setBookings] = useState([]);
+    const [filteredBookings, setFilteredBookings] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [currentPage, setCurrentPage] = useState(1);
+    const [bookingsPerPage] = useState(6);
+    const navigate = useNavigate();
+
+    
