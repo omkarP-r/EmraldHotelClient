281c4bd7767e114d3efc739f31136e35ff0d2039
omkarP-r <omkar29pawar@gmail.com>
2025-04-19 18:53:51 +0530

    edit room

---
+++ src/component/admin/EditRoomPage.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import ApiService from '../../service/ApiService';
+
+const EditRoomPage = () => {
+    const { roomId } = useParams();
+    const navigate = useNavigate();
+    const [roomDetails, setRoomDetails] = useState({
+        roomPhotoUrl: '',
+        roomType: '',
+        roomPrice: '',
+        roomDescription: '',
+    });
+   
