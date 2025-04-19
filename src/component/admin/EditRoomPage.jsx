8cc67185f2c1bb5d2d6a23c4220753acf7a4ef5d
omkarP-r <omkar29pawar@gmail.com>
2025-04-19 18:54:30 +0530

    update

--- src/component/admin/EditRoomPage.jsx
+++ src/component/admin/EditRoomPage.jsx
@@ -11,4 +11,57 @@ const EditRoomPage = () => {
         roomPrice: '',
         roomDescription: '',
     });
-   
+    const [file, setFile] = useState(null);
+    const [preview, setPreview] = useState(null);
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
+
+    useEffect(() => {
+        const fetchRoomDetails = async () => {
+            try {
+                const response = await ApiService.getRoomById(roomId);
+                setRoomDetails({
+                    roomPhotoUrl: response.room.roomPhotoUrl,
+                    roomType: response.room.roomType,
+                    roomPrice: response.room.roomPrice,
+                    roomDescription: response.room.roomDescription,
+                });
+            } catch (error) {
+                setError(error.response?.data?.message || error.message);
+            }
+        };
+        fetchRoomDetails();
+    }, [roomId]);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setRoomDetails(prevState => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
+
+    const handleFileChange = (e) => {
+        const selectedFile = e.target.files[0];
+        if (selectedFile) {
+            setFile(selectedFile);
+            setPreview(URL.createObjectURL(selectedFile));
+        } else {
+            setFile(null);
+            setPreview(null);
+        }
+    };
+
+
+    const handleUpdate = async () => {
+        try {
+            const formData = new FormData();
+            formData.append('roomType', roomDetails.roomType);
+            formData.append('roomPrice', roomDetails.roomPrice);
+            formData.append('roomDescription', roomDetails.roomDescription);
+
+            if (file) {
+                formData.append('photo', file);
+            }
+
+            
