aa40b007ed05be6e84f344aa7ed12a27f41594d2
omkarP-r <omkar29pawar@gmail.com>
2025-04-24 18:42:02 +0530

    edit booking page

---
+++ src/component/admin/EditBookingPage.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import ApiService from '../../service/ApiService'; // Assuming your service is in a file called ApiService.js
+
+const EditBookingPage = () => {
+    const navigate = useNavigate();
+    const { bookingCode } = useParams();
+    const [bookingDetails, setBookingDetails] = useState(null); // State variable for booking details
+    const [error, setError] = useState(null); // Track any errors
+    const [success, setSuccessMessage] = useState(null); // Track any errors
+
+
+
+   
