1cd887f248d6badd1ef42317ea801171dbb68e05
omkarP-r <omkar29pawar@gmail.com>
2025-04-27 18:55:54 +0530

    admin page

---
+++ src/component/admin/AdminPage.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import ApiService from '../../service/ApiService';
+
+const AdminPage = () => {
+    const [adminName, setAdminName] = useState('');
+    const navigate = useNavigate();
+
+    
