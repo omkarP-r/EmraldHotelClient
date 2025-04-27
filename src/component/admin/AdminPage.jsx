70474e454513c19eadcadf235309258583c9792f
omkarP-r <omkar29pawar@gmail.com>
2025-04-27 18:56:32 +0530

    admin name

--- src/component/admin/AdminPage.jsx
+++ src/component/admin/AdminPage.jsx
@@ -6,4 +6,17 @@ const AdminPage = () => {
     const [adminName, setAdminName] = useState('');
     const navigate = useNavigate();
 
+    useEffect(() => {
+        const fetchAdminName = async () => {
+            try {
+                const response = await ApiService.getUserProfile();
+                setAdminName(response.user.name);
+            } catch (error) {
+                console.error('Error fetching admin details:', error.message);
+            }
+        };
+
+        fetchAdminName();
+    }, []);
+
     
